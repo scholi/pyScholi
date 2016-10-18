89262b655f60320e8eacaffa14d7c7c2690f023e
Scholder <olivier.scholder@empa.ch>
2016-10-18 15:47:28 +0200

    First init

---
+++ aa.py
@@ -0,0 +1,9 @@
+def printTable(t):
+	Ncols=len(t[0])
+	Lcol=[0]*Ncols
+	for x in t:
+		for i in range(Ncols):
+			Lcol[i]=max(Lcol[i],len(x[i]))
+	for j,x in enumerate(t):
+		print("  ".join([u"{:"+['.','_'][j%2]+"<"+str(Lcol[i]+4)+"}" for i in range(Ncols)]).format(*x))
+	
