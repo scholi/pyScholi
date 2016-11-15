fc8965e8701736e87f17c5e451b6dfe47c81f995
Olivier <o.scholder@gmail.com>
2016-11-15 21:29:10 +0100

    Fix table ascii for numbers

--- aa.py
+++ aa.py
@@ -3,7 +3,7 @@ def printTable(t):
 	Lcol=[0]*Ncols
 	for x in t:
 		for i in range(Ncols):
-			Lcol[i]=max(Lcol[i],len(x[i]))
+			Lcol[i]=max(Lcol[i],len(repr(x[i])))
 	for j,x in enumerate(t):
 		print("  ".join([u"{:"+['.','_'][j%2]+"<"+str(Lcol[i]+4)+"}" for i in range(Ncols)]).format(*x))
 	
